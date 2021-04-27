40836e2bf69cef9bf16e05b6393c1f6d4fa1f6bf
Chamano Ramashia <chamano.sr@gmail.com>
2021-04-27 12:27:21 +0200

    fetching data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import './App.css';
+import requests from './request';
+import Row from './Row'
+import request from './request'
 
 function App() {
   return (
     <div className="App">
       <h1>Hey Chamano, lets build</h1>
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+      
     </div>
   );
 }
