d7d2ba4978c80a9b6882c728eddb84ad6d8ece94
Chamano Ramashia <chamano.sr@gmail.com>
2021-04-27 11:32:02 +0200

    mdtb api keys setup, firebase, axios setup

---
+++ src/request.js
@@ -0,0 +1,14 @@
+const API_KEY = "53aff48fb600f9ba6d011e9810d84563";
+
+const requests = {
+    fetchTrending:`/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals:`/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated:`/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies:`/discover/movies?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies:`/discover/movies?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies:`/discover/movies?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies:`/discover/movies?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries:`/discover/movies?api_key=${API_KEY}&with_genres=99`,
+}
+
+export default requests
