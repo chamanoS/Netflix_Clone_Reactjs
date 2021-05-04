e35b791824eac65729e454deb1221c2d78b21887
Chamano Ramashia <chamano.sr@gmail.com>
2021-05-04 09:51:37 +0200

    styling

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react'
 import './App.css';
 import requests from './request';
 import Row from './Row'
-import request from './request'
 
 function App() {
   return (
--- src/Row.js
+++ src/Row.js
@@ -1,32 +1,38 @@
-import React,{useState,useEffect} from 'react'
-import axios from './axios'
-
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import "./Row.css";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
-function Row({title, fetchUrl}) {
-    const [movies, setMovies] = useState([]);
+function Row({ title, fetchUrl }) {
+  const [movies, setMovies] = useState([]);
 
-    useEffect(() =>{
-        async function fetchData(){
-            const request = await axios.get(fetchUrl);
-            setMovies(request.data.results)
-            return request
-        }
-        fetchData();
-    }, [fetchUrl])
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
 
-        console.table(movies)
-    return (
-        <div className="row">
-            <h2>{title}</h2>
-            <div className="row__posters">
-                {movies.map(movie =>(
-                    <img className="row__poster" src={`${base_url}${movie.poster_path}`}alt={movie.name}/>
-                    ))}
-            </div>
-        </div>
-    )
+  console.table(movies);
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            className="row__poster"
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Row
+export default Row;
